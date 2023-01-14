d68299a44ad1a2060afe942650d6afc1b756007a
Ekaterina <dyuldina32@gmail.com>
2023-01-14 20:35:58 +0500

    sdelano 1 zadacha

---
+++ ex1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("vvedite a");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("vvedite b");
+int b = int.Parse(Console.ReadLine());
+int stepen = a;
+for (int i =1; i <= (b - 1); i++)
+{
+    stepen = stepen * a;
+}
+Console.WriteLine($"a v stepeni b {stepen}");
