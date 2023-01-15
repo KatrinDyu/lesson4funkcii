019125a5209a7123052b806cd92fa6549ebd6de3
Ekaterina <dyuldina32@gmail.com>
2023-01-15 19:32:02 +0500

    2 zadacha

--- ex1/Program.cs
+++ ex1/Program.cs
@@ -5,7 +5,7 @@ Console.WriteLine("vvedite b");
 int b = int.Parse(Console.ReadLine());
 int stepen = a;
 for (int i =1; i <= (b - 1); i++)
-{
-    stepen = stepen * a;
-}
+    {
+       stepen = stepen * a;
+    }
 Console.WriteLine($"a v stepeni b {stepen}");
---
+++ ex2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("vvedite chislo");
+int chislo = int.Parse(Console.ReadLine());
+int k = 0;
+for (int i= 0; i <= 3; i++)
+{
+   int ost = chislo % 10;
+    chislo = chislo / 10;
+    k = k + ost;
+
+}
+Console.WriteLine($"summa cifr {k}");
